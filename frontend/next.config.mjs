9535b0eb709a78d269c3f7cd80c5cd57dc5824a1
Solusiit-2025 <admin-user@solusiit.id>
2026-03-16 09:21:55 +0700

    Update next-config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -7,9 +7,45 @@ const withPWA = withPWAInit({
   skipWaiting: true,
 });
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://static.cloudflareinsights.com; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: http://localhost:5003 https://*; connect-src 'self' http://localhost:5003 https://*; font-src 'self' data:; frame-src 'self'; object-src 'none';"
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin'
+          }
+        ]
+      }
+    ]
+  }
 };
 
 export default withPWA(nextConfig);
