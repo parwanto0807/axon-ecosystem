5df2f24823848b1caa0577b30c189315c6656eca
Solusiit-2025 <admin-user@solusiit.id>
2026-07-31 03:03:44 +0700

    Update Error Login

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -45,14 +45,19 @@ export const authOptions: NextAuthOptions = {
     callbacks: {
         async session({ session, token }: any) {
             if (session.user) {
-                (session.user as any).id = token.sub
-                (session.user as any).role = token.role
-                (session.user as any).department = token.department
+                session.user.name = token.name || session.user.name
+                session.user.email = token.email || session.user.email
+                session.user.id = token.sub
+                session.user.role = token.role
+                session.user.department = token.department
             }
             return session
         },
         async jwt({ token, user }: any) {
             if (user) {
+                token.name = user.name
+                token.email = user.email
+                token.sub = user.id
                 token.role = user.role
                 token.department = user.department
             }
